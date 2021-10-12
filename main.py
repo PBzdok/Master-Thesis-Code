14eeb8edbe9f43ece40474ddd305672251f98628
Philipp Bzdok <philipp.bzdok@mailbox.org>
2021-10-12 17:59:17 +0200

    added generic features

--- main.py
+++ main.py
@@ -21,20 +21,46 @@ def load_projections_data(nrows):
 st.set_page_config(layout='wide')
 st.title('AI Assessment System')
 
-projections_data = load_projections_data(10000)
-reports_data = load_reports_data(10000)
+with st.expander('Overview'):
+    st.subheader('<Model Name>')
+    overview_l, overview_r = st.columns(2)
+    overview_l.text('<Import Date>')
+    overview_r.text('<Framework>')
 
-with st.expander('Projections data'):
-    st.subheader('Projections data')
+with st.expander('Model Description'):
+    st.subheader('Model Description')
+    st.write('Dolores sunt consequatur laborum. Et rem autem dolores qui assumenda. Sunt illum aut aspernatur maxime quo nostrum illo amet. Reprehenderit ut perspiciatis non alias aut accusantium et. Aspernatur tempore in adipisci pariatur earum et ut.')
+
+with st.expander('Capabilities'):
+    st.subheader('Capabilities')
+    st.markdown('* Dolores sunt consequatur laborum.')
+    st.markdown('* Dolores sunt consequatur laborum.')
+    st.markdown('* Dolores sunt consequatur laborum.')
+
+with st.expander('Standard Metrics'):
+    st.subheader('Standard Metrics')
+    metrics1, metrics2, metrics3, metrics4 = st.columns(4)
+    metrics1.metric(label='Max Error', value='<Value>')
+    metrics2.metric(label='Mean Error', value='<Value>')
+    metrics3.metric(label='Mean Squared Error', value='<Value>')
+    metrics4.metric(label='Root Mean Squared Error', value='<Value>')
+
+projections_data = load_projections_data(100)
+reports_data = load_reports_data(100)
+
+with st.expander('Browse Data'):
+    st.subheader('Browse Data')
     uid = st.selectbox('Select uid:', projections_data['uid'])
     img_url = projections_data['filename'][uid]
-    st.write(f'Selected URL: {img_url}')
     left_column, right_column = st.columns(2)
-    left_column.write(projections_data)
+    projections_table = left_column.dataframe(projections_data)
     if left_column.checkbox('Show metadata'):
-        left_column.write(reports_data.loc[reports_data['uid'] == uid])
-    right_column.image(f'./kaggle_x-ray/images/images_normalized/{img_url}')
+        st.table(reports_data.loc[reports_data['uid'] == uid])
+    if left_column.button('Show more'):
+        projections_data = load_projections_data(200)
+        projections_table.add_rows(projections_data)
+    right_column.image(f'./kaggle_x-ray/images/images_normalized/{img_url}',caption=f'{img_url}')
 
-with st.expander('Reports data'):
-    st.subheader('Reports data')
+with st.expander('Browse Metadata'):
+    st.subheader('Browse Metadata')
     st.write(reports_data)
