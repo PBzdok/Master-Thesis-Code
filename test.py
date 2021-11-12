21b993e6c55b20a2161223c90de2e4e3c1b82f45
Philipp Bzdok <philipp.bzdok@mailbox.org>
2021-11-12 17:41:36 +0100

    occlusion test

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torchxrayvision as xrv
-from captum.attr import visualization as viz
 from captum.attr import Occlusion
-from torchvision.transforms import transforms
 
-from data import load_rsna_dataset, calculate_rsna_metrics
+from data import load_rsna_dataset
 
 d_rsna = load_rsna_dataset()
 model = xrv.models.DenseNet(weights='densenet121-res224-rsna')
@@ -17,34 +16,21 @@ model = xrv.models.DenseNet(weights='densenet121-res224-rsna')
 # print(f'Recall: {result["recall"]}')
 # print(f'F1: {result["f1"]}')
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
-
-transform_normalize = transforms.Normalize((0.5,), (0.5,))
+i = 700
 
 occlusion = Occlusion(model)
 
-sample = d_rsna[42]
-transformed_img = transform(np.transpose(sample['img'], (1, 2, 0)))
-input = transform_normalize(transformed_img)
-input = input.unsqueeze(0)
+input1 = torch.from_numpy(np.expand_dims(d_rsna[i]['img'], axis=0)).float()
+pred_label_idx = model(input1).argmax()
 
-attributions_occ = occlusion.attribute(input,
-                                       strides=(1, 50, 50),
-                                       target=int(sample['lab'][0]),
-                                       sliding_window_shapes=(1, 60, 60),
-                                       baselines=0,
-                                       show_progress=True)
+attr = occlusion.attribute(input1,
+                           strides=(1, 30, 30),
+                           target=pred_label_idx,
+                           sliding_window_shapes=(1, 30, 30),
+                           baselines=0
+                           )
 
-print(attributions_occ.squeeze().cpu().detach().numpy().shape)
-print(transformed_img.squeeze().cpu().detach().numpy().shape)
-
-_ = viz.visualize_image_attr_multiple(np.transpose(np.expand_dims(attributions_occ.squeeze().cpu().detach().numpy(), axis=0), (1, 2, 0)),
-                                      np.transpose(np.expand_dims(transformed_img.squeeze().cpu().detach().numpy(), axis=0), (1, 2, 0)),
-                                      ["original_image", "heat_map"],
-                                      ["all", "positive"],
-                                      show_colorbar=True,
-                                      outlier_perc=2,
-                                      use_pyplot=True
-                                      )
+plt.imshow(input1[0, 0, :, :])
+plt.contourf(attr[0, 0, :, :], alpha=0.6)
+plt.colorbar()
+plt.show()
