7d64d555866faba5fb6263fab0eea7b0adc45ca2
Philipp Bzdok <philipp.bzdok@mailbox.org>
2021-11-12 16:14:57 +0100

    occlusion test

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
 import torchxrayvision as xrv
-import viz as viz
+from captum.attr import visualization as viz
 from captum.attr import Occlusion
+from torchvision.transforms import transforms
 
-from data import load_rsna_dataset
+from data import load_rsna_dataset, calculate_rsna_metrics
 
 d_rsna = load_rsna_dataset()
 model = xrv.models.DenseNet(weights='densenet121-res224-rsna')
@@ -16,21 +17,34 @@ model = xrv.models.DenseNet(weights='densenet121-res224-rsna')
 # print(f'Recall: {result["recall"]}')
 # print(f'F1: {result["f1"]}')
 
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+transform_normalize = transforms.Normalize((0.5,), (0.5,))
+
 occlusion = Occlusion(model)
 
 sample = d_rsna[42]
+transformed_img = transform(np.transpose(sample['img'], (1, 2, 0)))
+input = transform_normalize(transformed_img)
+input = input.unsqueeze(0)
 
-attributions_occ = occlusion.attribute(torch.from_numpy(sample["img"]).unsqueeze(0),
-                                       strides=(1, 8, 8),
+attributions_occ = occlusion.attribute(input,
+                                       strides=(1, 50, 50),
                                        target=int(sample['lab'][0]),
-                                       sliding_window_shapes=(1, 15, 15),
-                                       baselines=0)
+                                       sliding_window_shapes=(1, 60, 60),
+                                       baselines=0,
+                                       show_progress=True)
 
-_ = viz.visualize_image_attr_multiple(np.transpose(attributions_occ.squeeze().cpu().detach().numpy(), (1, 2, 0)),
-                                      np.transpose(torch.from_numpy(sample["img"]).squeeze().cpu().detach().numpy(),
-                                                   (1, 2, 0)),
+print(attributions_occ.squeeze().cpu().detach().numpy().shape)
+print(transformed_img.squeeze().cpu().detach().numpy().shape)
+
+_ = viz.visualize_image_attr_multiple(np.transpose(np.expand_dims(attributions_occ.squeeze().cpu().detach().numpy(), axis=0), (1, 2, 0)),
+                                      np.transpose(np.expand_dims(transformed_img.squeeze().cpu().detach().numpy(), axis=0), (1, 2, 0)),
                                       ["original_image", "heat_map"],
                                       ["all", "positive"],
                                       show_colorbar=True,
                                       outlier_perc=2,
+                                      use_pyplot=True
                                       )
