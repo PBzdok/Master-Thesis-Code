c20cc97d2e87e1ef3f0fdb6cdeae0f2e970e822d
Philipp Bzdok <philipp.bzdok@mailbox.org>
2021-11-14 12:45:00 +0100

    generate 1000 occlusions with test script

--- test.py
+++ test.py
@@ -4,33 +4,42 @@ import torch
 import torchxrayvision as xrv
 from captum.attr import Occlusion
 
-from data import load_rsna_dataset
+from data import load_rsna_dataset, calculate_rsna_metrics
 
 d_rsna = load_rsna_dataset()
 model = xrv.models.DenseNet(weights='densenet121-res224-rsna')
 
-# df, result = calculate_rsna_metrics(model, d_rsna, force=True)
-#
-# print(f'Accuracy: {result["accuracy"]}')
-# print(f'Precision: {result["precision"]}')
-# print(f'Recall: {result["recall"]}')
-# print(f'F1: {result["f1"]}')
+df_predictions, metrics = calculate_rsna_metrics(model, d_rsna, force=False)
 
-i = 700
+print(f'Accuracy: {metrics["accuracy"]}')
+print(f'Precision: {metrics["precision"]}')
+print(f'Recall: {metrics["recall"]}')
+print(f'F1: {metrics["f1"]}')
 
 occlusion = Occlusion(model)
 
-input1 = torch.from_numpy(np.expand_dims(d_rsna[i]['img'], axis=0)).float()
-pred_label_idx = model(input1).argmax()
+for index, row in df_predictions.iterrows():
+    patient_id = row['patientid']
 
-attr = occlusion.attribute(input1,
-                           strides=(1, 30, 30),
-                           target=pred_label_idx,
-                           sliding_window_shapes=(1, 30, 30),
-                           baselines=0
-                           )
+    patient_index = d_rsna.csv[d_rsna.csv['patientid'] == patient_id].index.values[0]
+    print(patient_index)
 
-plt.imshow(input1[0, 0, :, :])
-plt.contourf(attr[0, 0, :, :], alpha=0.6)
-plt.colorbar()
-plt.show()
+    model_input = d_rsna[patient_index]['img']
+    model_input = np.expand_dims(model_input, axis=0)
+    model_input = torch.from_numpy(model_input).float()
+
+    pred_label_idx = model(model_input).argmax()
+
+    attr = occlusion.attribute(model_input,
+                               strides=(1, 30, 30),
+                               target=pred_label_idx,
+                               sliding_window_shapes=(1, 30, 30),
+                               baselines=0,
+                               show_progress=True
+                               )
+
+    plt.imshow(model_input[0, 0, :, :])
+    plt.contourf(attr[0, 0, :, :], alpha=0.5)
+    plt.colorbar()
+    plt.savefig(f'./data/kaggle-pneumonia-jpg/occlusion/{patient_id}.jpg', bbox_inches='tight', dpi=150)
+    plt.clf()
